d5443f08c9e2dbc7b64aaa4ff8c2df52caa568d8
BhanuThapa <bhanuthapa252@gmail.com>
2023-08-23 23:58:28 +0530

    values and data types

---
+++ values_and_datatype.js
@@ -0,0 +1,18 @@
+// String : 'this is string'
+
+// Number : 9, 10.5,NaN, bingInt 1024n
+
+// boolean : true, false
+
+// undefined : undefined
+
+// null : null
+
+// Symbol : ^~@
+
+// Object : Array: [1,2,3,'a']
+
+// Object : {
+//   name:'asdf',
+//   age:2,
+// }
